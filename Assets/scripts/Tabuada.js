6959390d77c2a6970c273b9fde22ba32860997a8
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-23 15:54:08 -0300

    implementado o front-end da tabuada

--- Assets/scripts/Tabuada.js
+++ Assets/scripts/Tabuada.js
@@ -1,1 +1,22 @@
-﻿let nro = document.getElementById('numero');
+﻿let dom = {
+    nro: document.getElementById('numero'),
+    btnImprimir: document.getElementById('imprimir'),
+    tabuada: document.getElementById('tabuada'),
+};
+
+/**
+ * @async
+ * @param {int} nro
+ * @returns Promise<string>
+ */
+async function getTabuada(nro) {
+    let server = await fetch(`/tabuada/${nro}`);
+    return await server.text();
+}
+dom.btnImprimir.onclick = async () => {
+    const nro = dom.nro.value.trim();
+    console.log(nro)
+    if (nro === "") return;
+    const tabuada = await getTabuada(parseInt(nro));
+    dom.tabuada.innerHTML = tabuada.replaceAll('>>><<<', '<br>')
+}
